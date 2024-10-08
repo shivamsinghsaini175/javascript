c3bff95bc1a6d08e280bab4e20f87d07371d3378
shivamsinghsaini175 <122208475+shivamsinghsaini175@users.noreply.github.com>
2024-10-08 09:50:03 +0000

    variable and datatypes

---
+++ 1basic/variable.js
@@ -0,0 +1,10 @@
+const  id=12334
+let acc="shivam"
+var pass="1234"
+city="jammu"  // another way of var 
+//id=2    //not possible
+let acctate;  // undefined
+
+console.table([id,acc,pass,city,acctate])    // print in table form 
+
+// prefer not to use var bcs of funtion scope
