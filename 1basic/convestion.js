009d91b5866d735b830a110b348a873f9b3e1e69
shivamsinghsaini175 <122208475+shivamsinghsaini175@users.noreply.github.com>
2024-10-08 10:04:33 +0000

    conversion1 of datatypes

---
+++ 1basic/convestion.js
@@ -0,0 +1,19 @@
+let score="33"
+
+let valueInterger=Number(score)
+console.log(typeof score);
+console.log(typeof valueInterger);
+
+// op is NAN when string is not a pure number eg : score ="333aab " then convert kaete NAN aaye ga 
+// null ko zero   mai convert kar dega 
+// undefined mai b NAN aa jaye ga
+// true=> 1
+// false=>0
+ let boo=1
+ let isboole=Boolean(boo)
+ console.log(isboole);
+ // booleen mai
+// " " =>false
+// "shivam"=> true
+
+ 
